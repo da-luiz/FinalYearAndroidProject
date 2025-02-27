6bac06356d436fe378a70ce29d0453f72bbca4e1
Eruezi Umuomo <you@example.com>
2025-02-27 14:43:36 +0100

    Added domain-branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.finalyearproject"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.finalyearproject"
